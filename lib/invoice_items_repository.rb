17735d8f65835f52e1369ea786b9dda5008720a9
Horacio Chavez <horacio.hch@gmail.com>
2014-06-30 18:48:06 -0600

    invoice_items repository, entry and test

---
+++ lib/invoice_items_repository.rb
@@ -0,0 +1,56 @@
+require          'csv'
+require_relative 'entry_invoice_items'
+
+class InvoiceItemsRepository
+
+  def self.load
+    data = CSV.open('test/fixtures/small_invoice_items.csv', headers: true, header_converters: :symbol)
+    rows = data.map do |row|
+      EntryInvoiceItems.new(row)
+    end
+    new(rows)
+  end
+
+  attr_reader :entries
+
+  def initialize(entries)
+    @entries = entries
+  end
+
+  def find_by_id(id)
+    entries.select { |entry| entry.id == id }
+  end
+
+  def find_by_item_id(item_id)
+    entries.detect { |entry| entry.item_id == item_id}
+  end
+
+  def find_all_by_item_id(item_id)
+    entries.select { |entry| entry.item_id == item_id }
+  end
+
+  def find_by_invoice_id(invoice_id)
+    entries.detect { |entry| entry.invoice_id == invoice_id}
+  end
+
+  def find_all_by_invoice_id(invoice_id)
+    entries.select {|entry| entry.invoice_id == invoice_id }
+  end
+
+  def find_by_quantity(quantity)
+    entries.detect { |entry| entry.quantity == quantity}
+  end
+
+  def find_all_by_quantity(quantity)
+    entries.select {|entry| entry.quantity == quantity }
+  end
+
+  def find_by_unit_price(unit_price)
+    entries.detect { |entry| entry.unit_price == unit_price}
+  end
+
+  def find_all_by_unit_price(unit_price)
+    entries.select {|entry| entry.unit_price == unit_price }
+  end
+  
+end
---
+++ test/invoice_items_repository_test.rb
@@ -0,0 +1,76 @@
+gem 'minitest', '~>5.2'
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/invoice_items_repository'
+require 'pry'
+
+class InvoicesRepositoryTest < Minitest::Test
+
+  attr_reader :repository
+
+  def setup
+    @repository ||= InvoiceItemsRepository.load
+  end
+
+  def test_find_by_id
+    entries = repository.find_by_id('8')
+    assert_equal 1, entries.length
+    entries.each do |entry|
+      assert_equal "8", entry.id
+    end
+  end
+
+  def test_find_by_item_id
+    entry = repository.find_by_item_id('528')
+    assert_equal "528", entry.item_id
+  end
+
+  def test_find_all_by_item_id
+    entries = repository.find_all_by_item_id('539')
+    assert_equal 3, entries.length
+    entries.each do |entry|
+      assert_equal "539", entry.item_id
+    end
+  end
+
+  def test_find_by_invoice_id
+    entry = repository.find_by_invoice_id('2')
+    assert_equal "2", entry.invoice_id
+  end
+
+  def test_find_all_by_invoice_id
+    entries = repository.find_all_by_invoice_id('3')
+    assert_equal 8, entries.length
+    entries.each do |entry|
+      assert_equal "3", entry.invoice_id
+    end
+  end
+
+  def test_find_by_quantity
+
+    entry = repository.find_by_quantity('5')
+    assert_equal "5", entry.quantity
+  end
+
+  def test_find_all_by_quantity
+    entries = repository.find_all_by_quantity('19')
+    assert_equal 3, entries.length
+    entries.each do |entry|
+      assert_equal "19", entry.quantity
+    end
+  end
+
+  def test_find_by_unit_price
+    entry = repository.find_by_unit_price('2000')
+    assert_equal "2000", entry.unit_price
+  end
+
+  def test_find_all_by_unit_price
+    entries = repository.find_all_by_unit_price('1999')
+    assert_equal 3, entries.length
+    entries.each do |entry|
+      assert_equal "1999", entry.unit_price
+    end
+  end
+
+end
