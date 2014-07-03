3bbf27deecdb8de150fb80e83422d254423746f2
Horacio Chavez <horacio.hch@gmail.com>
2014-07-03 11:21:09 -0600

    refactor sales_engine.rb

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -16,27 +16,41 @@ class Engine
   end
 
   def startup
-    if @test_mode
-      @merchant_repository     ||= MerchantRepository.new
-      @invoice_repository      ||=  InvoicesRepository.new
-      @item_repository         ||= ItemsRepository.new
-      @invoice_item_repository ||= InvoiceItemsRepository.new
-      @customer_repository     ||= CustomerRepository.new
-      @transaction_repository  ||= TransactionRepository.new
-    else
-      @merchant_repository       = MerchantRepository.new.load('test/fixtures/biggggg_items.csv')
-      @invoice_repository        = InvoicesRepository.new.load('test/fixtures/small_items.csv')
-      @item_repository           = ItemsRepository.new.load('test/fixtures/small_items.csv')
-      @invoice_item_repository   = InvoiceItemsRepository.new.load('test/fixtures/small_items.csv')
-      @customer_repository       = CustomerRepository.new.load('test/fixtures/small_items.csv')
-      @transaction_repository    = TransactionRepository.new.load('test/fixtures/small_items.csv')
+    case
+    when @test_mode then test_mode_repositories
+    else real_life_repositories
     end
+    
+    merchant_relationship
+    invoice_relationship
+  end
 
+  def test_mode_repositories
+    @merchant_repository     ||= MerchantRepository.new
+    @invoice_repository      ||=  InvoicesRepository.new
+    @item_repository         ||= ItemsRepository.new
+    @invoice_item_repository ||= InvoiceItemsRepository.new
+    @customer_repository     ||= CustomerRepository.new
+    @transaction_repository  ||= TransactionRepository.new
+  end
+
+  def real_life_repositories
+    @merchant_repository       = MerchantRepository.new.load('test/fixtures/biggggg_items.csv')
+    @invoice_repository        = InvoicesRepository.new.load('test/fixtures/small_items.csv')
+    @item_repository           = ItemsRepository.new.load('test/fixtures/small_items.csv')
+    @invoice_item_repository   = InvoiceItemsRepository.new.load('test/fixtures/small_items.csv')
+    @customer_repository       = CustomerRepository.new.load('test/fixtures/small_items.csv')
+    @transaction_repository    = TransactionRepository.new.load('test/fixtures/small_items.csv')
+  end
+
+  def merchant_relationship
     merchant_repository.all.each do |merchant|
       merchant.invoices = invoice_repository.find_all_by_merchant_id(merchant.id)
       merchant.items    = item_repository.find_all_by_merchant_id(merchant.id)
     end
+  end
 
+  def invoice_relationship
     invoice_repository.all.each do |invoice|
       invoice.transaction = transaction_repository.find_by_invoice_id(invoice.id)
     end
