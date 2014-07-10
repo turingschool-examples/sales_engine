720d640412deafbb0371567f3f43a4645949cd0c
Horacio Chavez <horacio.hch@gmail.com>
2014-07-10 00:46:07 -0600

    refactor: item_repository.rb add: item_repository_helper.rb

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -1,40 +1,21 @@
-require          'csv'
-require_relative 'items'
-require_relative 'repository'
-require_relative 'invoice_item'
-
-require_relative 'transaction'
-
-
+require_relative 'item_repository_helper'
 
 class ItemRepository < Repository
 
-
   def self.load(filename)
     objects = []
-    CSV.foreach(filename, headers: true, header_converters: :symbol) do |row|
-      objects << Item.new(row)
-    end
+    CSV.foreach(filename, headers: true, header_converters: :symbol) { |row| objects << Item.new(row) }
     new(objects)
   end
 
   attr_reader :objects
 
-  #most_revenue(x) returns the top x item instances ranked by total revenue generated
   def most_revenue(number)
-    top_revenue = objects.sort_by { |item| item.revenue }.reverse
-    top_revenue.first(number)
+    top_revenue = objects.sort_by { |item| item.revenue }.reverse.first(number)
   end
-    #if item.status?
-      #objects.invoice_item.sort_by { |item| -item.revenue }[0..number-1]
-    #end
-
 
   def paid_transactions
-    successuful_transactions = transactions.select { |invoice| transactions.status == 'success' }
-    successuful_transactions.map(&:invoice_id).flatten
+    transactions.select { |invoice| transactions.status == 'success' }.map(&:invoice_id).flatten
   end
-
-  #most_items(x) returns the top x item instances ranked by total number sold
 
 end
