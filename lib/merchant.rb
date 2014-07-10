a4cfb472f7b6f75fac6767de7691f26061c6c361
Horacio Chavez <horacio.hch@gmail.com>
2014-07-09 23:32:18 -0600

    refactor: merchant.rb

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -1,10 +1,9 @@
 class Merchant
 
-  attr_reader :id,
-              :name,
-              :created_at,
-              :updated_at
-
+  attr_reader   :id,
+                :name,
+                :created_at,
+                :updated_at
 
   attr_accessor :invoices,
                 :items
@@ -18,29 +17,18 @@ class Merchant
   end
 
   def revenue(date = nil)
-    amount     = BigDecimal.new("0")
-    @invoices.each do |invoice|
-      if date == nil || date == invoice.updated_at
-        amount += invoice.revenue
-      end
-    end
+    amount = BigDecimal.new("0")
+    @invoices.each {|invoice| amount += invoice.revenue if date == nil || date == invoice.updated_at }
     amount
   end
 
-
   def total_items_sold
-    shipped_invoice_items.reduce(0) do |sum, invoice_item|
-    sum += invoice_item.quantity.to_i
-    end
+    shipped_invoice_items.reduce(0) { |sum, invoice_item| sum += invoice_item.quantity.to_i }
   end
 
   def favorite_customer
     customers = invoices.collect { |invoice| invoice.customer if invoice.status? }.compact
     customers.group_by { |item| item }.values.max_by(&:size).first
-  end
-
-  def inspect
-    "#<#{self.class} #{@merchants.size} rows>"
   end
 
   def customers_with_pending_invoices
