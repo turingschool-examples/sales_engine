3dd047aa40a5b7e3cc8817c17d4acde234376d0f
Ephemere <objectivparadigm@gmail.com>
2014-08-18 17:17:48 -0600

    addingin class model for invoice item

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -8,12 +8,12 @@ class Transaction
               :updated_at
 
   def initialize(data)
-    @id          = data[:id]
-    @customer_id = data[:invoice_id]
-    @merchant_id = data[:credit_card_number]
-    @status      = data[:credit_card_expiration_date]
-    @result      = data[:result]
-    @created_at  = data[:created_at]
-    @updated_at  = data[:updated_at]
+    @id                          = data[:id]
+    @invoice_id                  = data[:invoice_id]
+    @credit_card_number          = data[:credit_card_number]
+    @credit_card_expiration_date = data[:credit_card_expiration_date]
+    @result                      = data[:result]
+    @created_at                  = data[:created_at]
+    @updated_at                  = data[:updated_at]
   end
 end
