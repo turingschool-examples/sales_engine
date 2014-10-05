e4619b395e1a8c537b8f7c8dd9a5e30ade269ab7
Chris Bynum <cbynum@cdbcap.com>
2014-10-05 13:28:39 -0600

    created and tested invoice

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -1,3 +1,38 @@
-class Invoice
+require 'bigdecimal'
+require 'bigdecimal/util'
+require 'time'
 
+class Invoice
+  attr_reader :id,
+              :customer_id,
+              :merchant_id,
+              :status,
+              :created_at,
+              :updated_at,
+              :repo
+
+  def initialize(data, repo)
+    @id =          data[:id].to_i
+    @customer_id = data[:customer_id].to_i
+    @merchant_id = data[:merchant_id].to_i
+    @status =      data[:status]
+    @created_at =  Time.parse(data[:created_at])
+    @updated_at =  Time.parse(data[:updated_at])
+    @repo = repo
+  end
+
+  def transactions
+  end
+
+  def invoice_items
+  end
+
+  def items
+  end
+
+  def customer
+  end
+
+  def merchant
+  end
 end
--- test/invoice_test.rb
+++ test/invoice_test.rb
@@ -1,12 +1,25 @@
 require_relative 'test_helper.rb'
 require_relative '../lib/invoice'
+require 'time'
 
 class InvoiceTest < Minitest::Test
   def setup
-    skip
+    data = {
+      id:           '1',
+      customer_id:  '2',
+      merchant_id:  '3',
+      status:       'shipped',
+      created_at:   '2012-03-27 14:53:59 UTC',
+      updated_at:   '2012-03-27 14:53:59 UTC'
+    }
+    @invoice = Invoice.new(data, nil)
   end
 
-  def test
-
+  def test_item_attribute_matches
+    assert_equal 1, @invoice.id
+    assert_equal 2, @invoice.customer_id
+    assert_equal 3, @invoice.merchant_id
+    assert_equal Time.parse('2012-03-27 14:53:59 UTC'), @invoice.created_at
+    assert_equal Time.parse('2012-03-27 14:53:59 UTC'), @invoice.updated_at
   end
 end
