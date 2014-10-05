2c25907f7324a244e6732ea07b332cb548f44196
Chris Bynum <cbynum@cdbcap.com>
2014-10-05 13:07:40 -0600

    merchant created and tested

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -1,7 +1,24 @@
+require 'time'
+
 class Merchant
+  attr_reader :id,
+              :name,
+              :created_at,
+              :updated_at,
+              :repo
+
+  def initialize(data, repo)
+    @id = data[:id].to_i
+    @name = data[:name]
+    @created_at = Time.parse(data[:created_at])
+    @updated_at = Time.parse(data[:updated_at])
+    @repo = repo
+  end
+
   def items
   end
 
   def invoices
   end
+
 end
