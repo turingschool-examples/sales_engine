749bd78a7f63aff780ed1ccf4f41d0a1685d492f
Sam Kuhlmann <samkuhlmann@gmail.com>
2014-10-06 17:26:27 -0600

    Added Invoice Item class, .gitignore and tests

---
+++ lib/invoice_item.rb
@@ -0,0 +1,20 @@
+class InvoiceItem
+	attr_reader :id,
+					  	:item_id,
+						  :invoice_id,
+						  :quantity,
+						  :unit_price,
+						  :created_at,
+						  :updated_at
+
+	def initialize(data)
+		@id         = data[:id]
+		@item_id    = data[:item_id]
+		@invoice_id = data[:invoice_id]
+		@quantity   = data[:quantity]
+		@unit_price = data[:unit_price]
+		@created_at = data[:created_at].split(" ")[0]
+		@updated_at = data[:updated_at].split(" ")[0]
+	end
+
+end
---
+++ test/invoice_item_test.rb
@@ -0,0 +1,51 @@
+require_relative 'test_helper'
+
+class InvoiceItemTest < Minitest::Test 
+
+	attr_reader :invoice_item
+
+	def setup
+		data = {id: 1,
+						item_id: 539,
+						invoice_id: 1,
+						quantity: 5,
+						unit_price: 13635,
+						created_at: "2012-03-27 14:54:09 UTC",
+						updated_at: "2012-03-27 14:54:09 UTC"
+						}
+
+		@invoice_item = InvoiceItem.new(data)
+	end
+
+	def test_invoice_item_has_an_id
+		setup
+		assert_equal 1, invoice_item.id
+	end
+
+	def test_invoice_item_has_an_item_id
+		setup
+		assert_equal 539, invoice_item.item_id
+	end
+
+	def test_invoice_item_has_an_invoice_id
+		setup
+		assert_equal 1, invoice_item.invoice_id
+	end
+
+	def test_invoice_item_has_a_quantity
+		setup
+		assert_equal 5, invoice_item.quantity
+	end
+
+	def test_invoice_item_has_an_unit_price
+		setup
+		assert_equal 13635, invoice_item.unit_price
+	end
+
+	def test_invoice_item_has_metadata
+		setup
+		assert_equal "2012-03-27", invoice_item.created_at
+		assert_equal "2012-03-27", invoice_item.updated_at
+	end
+
+end
