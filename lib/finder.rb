6dc8745aff4d95d31b8812e0f5adec6cb303a36c
Sam Kuhlmann <samkuhlmann@gmail.com>
2014-10-07 11:39:34 -0600

    Commented out some notes

--- lib/finder.rb
+++ lib/finder.rb
@@ -1,14 +1,14 @@
 
 
 
-def find_by(attribute, value)
-	items.find { |record| record.send(attribute.to_sym) == value }
-end
+# def find_by(attribute, value)
+# 	items.find { |record| record.send(attribute.to_sym) == value }
+# end
 
 
-def find_by_all(attribute, value)
-	items.select { |record| record.send(attribute.to_sym) == value }
-end
+# def find_by_all(attribute, value)
+# 	items.select { |record| record.send(attribute.to_sym) == value }
+# end
 
 
-def find_by_first_name(value) find_by_all(first_name, value) end
+# def find_by_first_name(value) find_by_all(first_name, value) end
