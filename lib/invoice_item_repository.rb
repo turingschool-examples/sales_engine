3f188e1c4f80cb64ca69f5369b5f145da2ec0685
Greg Narajka <gregnar@gmail.com>
2014-10-07 11:19:53 -0600

    invoice item repo changes

--- lib/invoice_item_repository.rb
+++ lib/invoice_item_repository.rb
@@ -7,14 +7,6 @@ class InvoiceItemRepository < Repository
     @repo = []
   end
 
-  def populate_repository(parsed_csv, InvoiceItem.new)
-    parsed_csv.map do |hash|
-      hash.map do |key, value|
-        instance_class.key = value
-      end
-    end
-  end
-
   def find_by_id(arg); finder(:id, arg); end
   def find_by_item_id(arg); finder(:item_id, arg); end
   def find_by_invoice_id(arg); finder(:invoice_id, arg); end
@@ -23,4 +15,4 @@ class InvoiceItemRepository < Repository
   def find_by_created_at(arg); finder(:created_at, arg); end
   def find_by_updated_at(arg); finder(:updated_at, arg); end
 
-end
+endaw4
