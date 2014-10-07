78e9a75edd7881d3636899a16f25622dd3e06de3
Sam Kuhlmann <samkuhlmann@gmail.com>
2014-10-07 16:58:50 -0600

    Added ItemRepo Tests

--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -5,96 +5,99 @@ class ItemRepositoryTest < Minitest::Test
 
 	def setup
 		file_path = "./test/support/test_items.csv"
-		@item_repository = ItemRepository.new(file_path)
+		parser = Parser.new(file_path)
+		items = parser.rows.map {|row| Item.new(row)}
+		@item_repository = ItemRepository.new(items)
 	end
 
 	def test_instantiates_with_items_array
 		setup
 
-		assert_equal 14, item_repository.items.count
+		assert_equal 14,item_repository.items.count
 	end
 
 	def test_returns_all_items
 		setup
+		results = item_repository.all
 
-		assert_equal item_repository.items.count, item_repository.all.count
+		assert_equal item_repository.items.count, item_repository.results.count
 	end
 
 	def test_returns_a_random_item
 		setup
-		random_instance = item_repository.random
+		results = item_repository.random
 
-		assert_equal 1, random_instance.count
+		assert_equal 1, results.count
 	end
 
 	def test_finds_by_missing_value_returns_an_empty_array
 		setup
-		found_item = item.repository.find_by_id("1000000")
+		results = item_repository.find_by_id("1000000")
 
-		assert_equal [], found_item
+		assert_equal [], results
 	end
 
 		def test_finds_all_by_missing_value_returns_an_empty_array
 		setup
-		found_item = item.repository.find_all_by_id("1000000")
+		results = item_repository.find_all_by_id("1000000")
 
-		assert_equal [], found_item
+		assert_equal [], results
 	end
 
 	def test_finds_by_id
 		setup
-		found_item = item.repository.find_by_id("4")
+		results = item_repository.find_by_id("4")
 
-		assert_equal "item nemo facere", found_item.name
+		assert_equal "item nemo facere", results.name
 	end	
 
 	def test_finds_by_name
 		setup
-		found_item = item.repository.find_by_name("Item Expedita Fuga")
+		results = item_repository.find_by_name("Item Expedita Fuga")
 
-		assert_equal "7", found_item.id
+		assert_equal "7", results.id
 	end	
 
 	def test_finds_by_description
 		setup
-		found_item = item.repository.find_by_description("sunt eum id eius magni consequuntur delectus veritatis. quisquam laborum illo ut ab. ducimus in est id voluptas autem.")
+		results = item_repository.find_by_description("sunt eum id eius magni consequuntur delectus veritatis. quisquam laborum illo ut ab. ducimus in est id voluptas autem.")
 
-		assert_equal "4", found_item.id
+		assert_equal "4", results.id
 	end	
 
 	def test_finds_by_unit_price
 		setup
-		found_item = item.repository.find_by_unit_price("67076")
+		results = item_repository.find_by_unit_price("67076")
 
-		assert_equal "2", found_item.id
+		assert_equal "2", results.id
 	end	
 
 	def test_finds_by_merchant_id
 		setup
-		found_item = item.repository.find_by_merchant_id("1")
+		results = item_repository.find_by_merchant_id("1")
 
-		assert_equal "1", found_item.id
+		assert_equal "1", results.id
 	end
 
 	def test_finds_by_create_date
 		setup
-		found_item = item.repository.find_by_merchant_id("2012-03-27")
+		results = item_repository.find_by_merchant_id("2012-03-27")
 
-		assert_equal "1", found_item.id
+		assert_equal "1", results.id
 	end
 
 	def test_finds_by_update_date
 		setup
-		found_item = item.repository.find_by_merchant_id("2012-03-27")
+		results = item_repository.find_by_merchant_id("2012-03-27")
 
-		assert_equal "1", found_item.id
+		assert_equal "1", results.id
 	end
 
 	def test_finds_all_by_merchant_id
 		setup
-		found_items = item.repository.find_all_by_id("1")
+		results = item_repository.find_all_by_id("1")
 
-		assert_equal 14, found_items.count
+		assert_equal 14, results.count
 		assert_equal "item itaque consequatur", found_items[13].name
 	end
 end
