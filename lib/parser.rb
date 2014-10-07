bf0e87abe66275d62f08f2503d69d0718cca21e6
Sam Kuhlmann <samkuhlmann@gmail.com>
2014-10-07 07:07:01 -0600

    Added parser logic, passing 1st parser test

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,22 +1,11 @@
 require 'csv'
 
 class Parser
-  attr_reader :items
-
+  attr_reader :parsed_array
+  
   def initialize(repository_type, file_path)
-    @file_path = file_path
-    @repository_type = repository_type
     data = CSV.open(file_path, headers: true, header_converters: :symbol)
-
-# somehow use repository_type variable to call the correct method below
-
-# add class variable and drop that in below
-
+    @parsed_array = data.map {|row| repository_type.new(row) }
   end
-
 end
-#all methods
-  # @items = data.map {|row| Item.new(row)}
-  # @merchants = data.map {|row| Merchants.new(row)}
 
-# end
