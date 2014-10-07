3ed30254935f744c3b9dfee4c3b34e80b285b73d
Greg Narajka <gregnar@gmail.com>
2014-10-07 16:23:00 -0600

    made item repo test

--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -8,7 +8,16 @@ class ItemRepositoryTest < Minitest::Test
     raw_csv = CSVParser.new.load_csv('./data/items_test.csv')
     item_repo = ItemRepository.new
     item_repo.populate_repository(raw_csv, Item)
-    assert_instance_of Item, item_repo.repo[1], "not instance of Item"
+    assert_instance_of Item, item_repo.repo[0], "not instance of Item"
+    assert_instance_of Item, item_repo.repo[-1], "not instance of Item"
   end
 
+  def test_find_by_id
+    skip
+  end
+
+  def find_by_name
+  end
+
+  def find_by_description
 end
