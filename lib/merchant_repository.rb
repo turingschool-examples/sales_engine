6e1c37f2437b50f5a02627468877e92f027acf08
Alex Jensen <96dalex@gmail.com>
2014-10-06 18:03:36 -0600

    move define loop to module

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -7,14 +7,8 @@ class MerchantRepository
     @entries = merchants
   end
 
-  %w(id name created_at updated_at).each do |attribute|
-    define_method "find_by_#{attribute}" do |value|
-      find_by(__method__[8..-1], value)
-    end
-
-    define_method "find_all_by_#{attribute}" do |value|
-      find_all_by(__method__[12..-1], value)
-    end
+  def attributes
+    %w(id name created_at updated_at)
   end
-
+  
 end
--- lib/repository.rb
+++ lib/repository.rb
@@ -1,6 +1,16 @@
 module Repository
   attr_reader :entries
 
+  attributes.each do |attribute|
+    define_method "find_by_#{attribute}" do |value|
+      find_by(__method__[8..-1], value)
+    end
+
+    define_method "find_all_by_#{attribute}" do |value|
+      find_all_by(__method__[12..-1], value)
+    end
+  end
+
   def all
     entries
   end
