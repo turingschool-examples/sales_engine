4217104e45011e36176bd4ad9a757f20868ffd58
Chandra Carney <chandracarney@me.com>
2014-10-07 11:38:41 -0600

    added merchant/repository, item/repository, and transaction/repository

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -1,4 +1,23 @@
-class ItemRepository
+require_relative 'repository'
+require_relative 'item'
+
+class ItemRepository < Repository
+
+  attr_reader :repo
+
+  def initialize
+    @repo = []
+  end
+
+  def find_by_id(arg); finder(:id, arg); end
+  def find_by_name(arg); finder(:name, arg); end
+  def find_by_description(arg); finder(:description, arg); end
+  def find_by_unit_price(arg); finder(:unit_price, arg); end
+  def find_by_merchant_id(arg); finder(:merchant_id, arg); end
+  def find_by_created_at(arg); finder(:created_at, arg); end
+  def find_by_updated_at(arg); finder(:updated_at, arg); end
+
+end
 
   def most_revenue(arg)
   end
--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -1,4 +1,18 @@
-class MerchantRepository
+require_relative 'repository'
+require_relative 'merchant'
+
+class MerchantRepository < Repository
+
+  attr_reader :repo
+
+  def initialize
+    @repo = []
+  end
+
+  def find_by_id(arg); finder(:id, arg); end
+  def find_by_name(arg); finder(:name, arg); end
+  def find_by_created_at(arg); finder(:created_at, arg); end
+  def find_by_updated_at(arg); finder(:updated_at, arg); end
 
   def most_revenue(arg)
   end
@@ -6,7 +20,7 @@ class MerchantRepository
   def most_items(arg)
   end
 
-  def revenue(date arg)
+  def revenue(date, arg)
   end
-  
+
 end
