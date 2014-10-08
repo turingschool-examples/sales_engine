f723660ee7ab09446eb199f5878dae15e32162ef
Chandra Carney <chandracarney@me.com>
2014-10-08 15:53:10 -0600

    added find_all_by

--- lib/transaction_repository.rb
+++ lib/transaction_repository.rb
@@ -17,6 +17,14 @@ class TransactionRepository < Repository
   def find_by_created_at(arg); find_by(:created_at, arg); end
   def find_by_updated_at(arg); find_by(:updated_at, arg); end
 
+  def find_all_by_id(arg); find_all_by(:id, arg); end
+  def find_all_by_invoice_id(arg); find_all_by(:invoice_id, arg); end
+  def find_all_by_credit_card_number(arg); find_all_by(:credit_card_number, arg); end
+  def find_all_by_credit_card_expiration_date(arg); find_all_by(:credit_card_expiration_date, arg); end
+  def find_all_by_result(arg); find_all_by(:result, arg); end
+  def find_all_by_created_at(arg); find_all_by(:created_at, arg); end
+  def find_all_by_updated_at(arg); find_all_by(:updated_at, arg); end
+
   def find_invoice_by_invoice_id(invoice_id)
     sales_engine.find_invoice_by_invoice_id(invoice_id)
   end
