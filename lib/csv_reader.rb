79909d7ab274ee80a0d69b2fc33e658b9faca7a6
Herbert Joseph <techniquea2z@gmail.com>
2014-10-08 13:08:57 -0600

    csv reader works now ;)

--- lib/csv_reader.rb
+++ lib/csv_reader.rb
@@ -1,7 +1,15 @@
 require 'csv'
+require 'date'
+require 'bigdecimal'
+require 'bigdecimal/util'
+
 class CSVReader
-  def read(file_name)
+  def read(file_name, conversion=false)
+    csv = Array.new
     file_name = File.join "data", file_name
-    csv       = CSV.table(file_name, :header_converters => :symbol)
+    CSV.foreach(file_name, headers: true ,header_converters: :symbol, converters: :string_to_date_time) do |row|
+      csv << row.to_hash
+    end
+    csv
   end
 end
