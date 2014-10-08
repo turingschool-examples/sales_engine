826b9eb2ff57da8f419e3850776245e00acf0fc5
Sam Kuhlmann <samkuhlmann@gmail.com>
2014-10-08 14:46:17 -0600

    invoices are done

--- test/invoice_repository_test.rb
+++ test/invoice_repository_test.rb
@@ -24,7 +24,7 @@ class InvoiceRepositoryTest < Minitest::Test
 	def test_finds_by_id
 		results = invoice_repository.find_by_id("4")
 
-		assert_equal "26", results.merchant_id
+		assert_equal "33", results.merchant_id
 	end	
 
 	def test_finds_by_customer_id
@@ -40,7 +40,7 @@ class InvoiceRepositoryTest < Minitest::Test
 	end	
 
 	def test_finds_by_status
-		results = invoice_repository.find_by_merchant_id("shipped")
+		results = invoice_repository.find_by_status("shipped")
 
 		assert_equal "1", results.id
 	end	
@@ -58,35 +58,51 @@ class InvoiceRepositoryTest < Minitest::Test
 	end
 
 	def test_finds_all_by_customer_id
-		results = merchant_repository.find_all_by_customer_id("3")
+		results = invoice_repository.find_all_by_customer_id("3")
 
 		assert_equal 4, results.count
 		assert_equal "10", results[0].id
 	end
 
 	def test_finds_by_missing_value_returns_an_empty_array
-		skip
 		results = invoice_repository.find_by_id("1000000")
 
 		assert_empty results
 	end
 
 	def test_finds_all_by_missing_value_returns_an_empty_array
-		skip
-		assert_empty invoice_repository.find_all_by_id("1000000")
+		results = invoice_repository.find_all_by_id("1000000")
 
 		assert_empty results
 	end
 
 	def test_delegates_find_transactions_for_to_sales_engine
 		sales_engine.expect(:find_transactions_by_invoice, [], ["1"])
-		merchant_repository.find_transactions_for("1")
+		invoice_repository.find_transactions_for("1")
 		sales_engine.verify
 	end
 
 	def test_delegates_find_invoice_items_for_to_sales_engine
 		sales_engine.expect(:find_invoice_items_by_invoice, [], ["1"])
-		merchant_repository.find_invoice_items_for("1")
+		invoice_repository.find_invoice_items_for("1")
+		sales_engine.verify
+	end
+
+	def test_delegates_find_items_for_to_sales_engine
+		sales_engine.expect(:find_items_by_invoice, [], ["1"])
+		invoice_repository.find_items_for("1")
+		sales_engine.verify
+	end
+
+	def test_delegates_find_customer_for_to_sales_engine
+		sales_engine.expect(:find_customer_by_invoice, [], ["1"])
+		invoice_repository.find_customer_for("1")
+		sales_engine.verify
+	end
+
+	def test_delegates_find_merchant_for_to_sales_engine
+		sales_engine.expect(:find_merchant_by_invoice, [], ["1"])
+		invoice_repository.find_merchant_for("1")
 		sales_engine.verify
 	end
 end
