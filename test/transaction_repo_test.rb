02e5dcbd3c788fab5d013d02780ea67aa9fe478e
Chandra Carney <chandracarney@me.com>
2014-10-08 13:39:42 -0600

    fixed small stuff in tests

--- test/transaction_repo_test.rb
+++ test/transaction_repo_test.rb
@@ -1,14 +1,15 @@
 require_relative 'test_helper'
 require './lib/transaction_repository'
+require './lib/sales_engine'
 
 class TransactionRepoTest < Minitest::Test
 
-def test_repo_gets_populated
-  raw_csv = CSVParser.new.load_csv('./data/transactions_test.csv')
-  trans_repo = TransactionRepository.new
-  trans_repo.populate_repository(raw_csv, Transaction)
-  assert_instance_of Transaction, trans_repo.repo[0], "not instance of Transaction"
-  assert_instance_of Transaction, trans_repo.repo[-1], "not instance of Transaction"
-end
+  def test_repo_gets_populated
+    raw_csv = CSVParser.new.load_csv('./data/transactions_test.csv')
+    trans_repo = TransactionRepository.new(SalesEngine.new)
+    trans_repo.populate_repository(raw_csv, Transaction)
+    assert_instance_of Transaction, trans_repo.repo[0], "not instance of Transaction"
+    assert_instance_of Transaction, trans_repo.repo[-1], "not instance of Transaction"
+  end
 
 end
