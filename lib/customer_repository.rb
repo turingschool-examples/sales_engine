b7198ef95a11e78bb167e7538e76c1df450dede3
justin <justin.ryan.holmes@icloud.com>
2015-04-22 11:30:15 -0600

    changed rakefile, added invoice repo and tests

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -1,23 +1,21 @@
 require 'csv'
 require_relative "customer"
-require 'pry'
 
 class CustomerRepository
   attr_accessor :customers
 
   def make_customers_array
     contents = CSV.open "./data/customers.csv", headers: true, header_converters: :symbol
-
-
+    
     @customers = contents.map do |row|
-                    customer = Customer.new
-                    customer.id = row[:id]
+                    customer            = Customer.new
+                    customer.id         = row[:id]
                     customer.first_name = row[:first_name]
-                    customer.last_name = row[:last_name]
+                    customer.last_name  = row[:last_name]
                     customer.created_at = row[:created_at]
                     customer.updated_at = row[:updated_at]
                     customer
-                end
+    end
   end
 
   def random
@@ -39,11 +37,4 @@ class CustomerRepository
   def find_all_by_last_name(last_name)
     @customers.select { |customer| customer.last_name == last_name }
   end
-
 end
-
-c = CustomerRepository.new
-c.make_customers_array
-
-puts c.find_by_first_name("Ashly")
-
