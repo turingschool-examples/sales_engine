c0aa8094217eea9495dc429f3edbda34ff0ecada
Sally MacNicholas <smacnicholas@gmail.com>
2015-04-27 16:20:15 -0600

    updated customer tests

--- test/customer_test.rb
+++ test/customer_test.rb
@@ -23,4 +23,12 @@ class CustomerTest < Minitest::Test
     assert_equal 2, c.invoices.length
     assert_equal true, c.invoices.first.is_a?(Invoice)
   end
+
+  def test_transactions_returns_collection_of_transactions
+    assert_equal 2, c.transactions.length
+  end
+
+  def test_favorite_merchant_returns_fave_merchant
+    assert_equal 26, c.favorite_merchant.id
+  end
 end
